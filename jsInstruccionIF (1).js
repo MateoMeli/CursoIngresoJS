2fa05f4ffbd691f035c6915554ec903f97f2e5d4
MateoMeli <52387813+MateoMeli@users.noreply.github.com>
2019-07-08 13:09:02 -0300

    Add files via upload

---
+++ jsInstruccionIF (1).js
@@ -0,0 +1,17 @@
+function mostrar()
+{   var edad;
+
+    edad = document.getElementById("edad").value;
+    edad = parseInt(edad);
+
+    if(edad != 15)
+    {}
+    else
+    {
+        alert("nina bonita")
+    }
+//tomo la edad  
+
+
+
+}//FIN DE LA FUNCIÓN
