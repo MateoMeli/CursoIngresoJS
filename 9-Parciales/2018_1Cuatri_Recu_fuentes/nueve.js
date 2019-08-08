2d4d91e4b5b2e78a4209a47f3fc845ece33b347a
mateomeli <mateomeli7@gmail.com>
2019-08-08 13:48:37 -0300

    falta ultimo

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
@@ -1,30 +1,55 @@
-function mostrar()
-{
+function mostrar() {
     var animal;
     var peso;
     var temperatura;
     var seguir = "s";
+    var contpares = 0;
+    var animalmax;
+    var tempmax;
+    var pesomax;
+    var contador = 0;
+    var tempcero = 0;
+    var acumpeso = 0;
 
-    while(seguir == "s"){
+    while (seguir == "s") {
         animal = prompt("Ingrese el animal:");
-        peso = parseInt(prompt("Ingrese el peso entre 1 y 1000: " ));
-        while(isNaN(peso) || peso < 1 || peso > 1000){
+
+        peso = parseInt(prompt("Ingrese el peso entre 1 y 1000: "));
+        while (isNaN(peso) || peso < 1 || peso > 1000) {
             peso = parseInt(prompt("Ingrese un peso valido:"));
         }
+
         temperatura = parseInt(prompt("Ingresa la temperatura del hábitat: "));
-        while(isNaN(temperatura) || temperatura < 30 || temperatura > 30){
-            temperatura = parseInt(prompt("Ingresa una temperatura valida: "));    
+        while (isNaN(temperatura) || temperatura < -30 || temperatura > 30) {
+            temperatura = parseInt(prompt("Ingresa una temperatura valida: "));
         }
+
+        if((temperatura % 2) == 0){
+            contpares ++;
+        }
+
+        if(contador == 0){
+            animalmax = animal;
+            tempmax = temperatura;
+            pesomax = peso;
+        }
+        if(peso > pesomax){
+            animalmax = animal;
+            tempmax = temperatura;
+            pesomax = peso;
+        }
+        if(temperatura < 0){
+            tempcero ++;
+        }
+        acumpeso = peso + acumpeso;
+        contador ++;
         seguir = prompt("Quiere ingresar mas datos?");
     }
-//     Realizar el algoritmo que permita ingresar el nombre de un animal del zoológico,
-//      el peso el cual debe ser entre 1 y 1000 y la temperatura del hábitat
-//       (entre -30 y 30) hasta que el usuario quiera e informar al terminar el
-//        ingreso por document.write: 
-// a) La cantidad de temperaturas pares. 
-// b) El nombre y temperatura del animal más pesado 
-// c) La cantidad de animales que viven a menos de 0 grados. 
-// d) El promedio del peso de todos los animales.	
-// f) El peso máximo y el mínimo de todos los animales cuyas temperaturas sean bajo cero.
+    document.write("La cantidad de numeros pares ingresados fue: " + contpares + "<br>");
+    document.write("El nombre del animal mas pesado es: " + animalmax + " y la temperatura de su habitat es de: " + tempmax + "°C." + "<br>");
+    document.write("La cantidad de animales que viven a menos de 0°C es: " + tempcero + "<br>");
+    document.write("El promedio de todos los pesos ingresados es: " + acumpeso/contador + "<br>");
+    
+    // f) El peso máximo y el mínimo de todos los animales cuyas temperaturas sean bajo cero.
 
 }
